adf65d732183f2d877540a74cd7c8e037aa72081
Aybars Hasan Şalvarcı <aybarsalvarci44@gmail.com>
2025-02-08 00:33:02 +0300

    Dashboard view

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Back\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/layout', function () {
-    return view('back.dashboard');
+Route::prefix('/dashboard')->middleware('auth')->group(function(){
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
 });
+
